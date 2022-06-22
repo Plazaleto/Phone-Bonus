376dd1566e82a57d60bc51f0c3aadae3014e17b9
Tanya Pustovetova <plazaleto@yandex.ru>
2022-06-22 15:49:38 +0700

    4 commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int point = 1; // бонус 1 рубль за каждые 100 руб пополнения
+        int rubles = 1100; //  пополнение счета
+        int invoice = 100; // начальная сумма на счету
+        int bonus;
+        if (rubles <= 1000) {
+            bonus = 0;
+        } else {
+            bonus = rubles * point / 100;
+        }
+        int total = invoice + rubles + bonus;
+        System.out.println(total);
     }
 }
